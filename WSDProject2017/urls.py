5ed5e993af1fe0d1b5635f4929b6e55520cc451b
Alex Savia <alex.savia@aalto.fi>
2018-01-23 12:21:12 +0200

    fixed errors

--- WSDProject2017/urls.py
+++ WSDProject2017/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from homepage import views
+from gamespage import views as gameviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('games', gameviews.games, name='games'),
     
 ]
