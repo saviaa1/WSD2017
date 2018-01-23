8df0285498e69fcfdddea1c9085fb14ba746d1cc
Alex Savia <alex.savia@aalto.fi>
2018-01-23 10:00:36 +0200

    Added a very basic homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+def index(request):
+    num_games = 0
+    num_customers = 0
+    return render(
+        request,
+        "index.html",
+        context={"num_games":num_games, "num_customers":num_customers},
+    )
