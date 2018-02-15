f177ed295b572102016a2deceb2dc589072c8453
Alex Savia <alex.savia@aalto.fi>
2018-02-15 14:47:59 +0200

    Gamelist page style done.

--- gamelist/views.py
+++ gamelist/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator  # EmptyPage, PageNotAnInteger
 from .models import Game, GameCategories
 from django.http import Http404
 
@@ -7,21 +7,19 @@ from django.http import Http404
 # Create your views here.
 class GameListView(TemplateView):
     template_name = "game_list.html"
+
     def get_context_data(self, **kwargs):
         # context = super(GameListView, self).get_context_data(**kwargs)
         products = Game.objects.all().order_by('id')
         cat = kwargs.get("category", None)
         if cat:
-            try:
-                cat = cat.upper()
-            except:
-                raise Http404("No such category")
+            cat = cat.upper()
             if cat not in GameCategories.categoriesList:
                 raise Http404("No such category")
             else:
-                products = products.filter(category = cat)
+                products = products.filter(category=cat)
 
-        paginator = Paginator(products, 1)
+        paginator = Paginator(products, 6)
         page = self.request.GET.get("page")
         currentPageSet = paginator.get_page(page)
         return {"gameQSet": currentPageSet}
